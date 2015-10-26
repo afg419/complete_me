e7c0ac5b54ade75e2c892b7b8cf0dc931ecf51b0
Aaron Greenspan <afg419@gmail.com>
2015-10-26 17:48:00 -0600

    adding words potentially

--- lib/complete_me.rb
+++ lib/complete_me.rb
@@ -8,7 +8,7 @@ end
 
 class CompleteMe
 
-  attr_accessor :root, :links,:word?
+  attr_accessor :root, :links, :word
 
   def initialize(data = nil)
     @root = data
@@ -17,20 +17,31 @@ class CompleteMe
 
   def add_word(string, depth = 0)
 
-    if !data && depth == (string.length - 1)
-      data = string
+    if !root && depth == (string.length)
+      self.root = string
       self.word = true
-    elsif !data
-      data = string[0..depth]
-      links[depth] ||= CompleteMe.new
-      links[depth].add_word(string,depth +1)
+    elsif !root
+      self.root = string[0..depth-1]
+      links[string[depth]] ||= CompleteMe.new
+      links[string[depth]].add_word(string,depth +1)
     elsif
-      links[depth] ||= CompleteMe.new
-      links[depth].add_word(string,depth + 1)
+      links[string[depth]] ||= CompleteMe.new
+      links[string[depth]].add_word(string,depth + 1)
     end
 
   end
-
-
+  # 
+  # def link_forward(char)
+  #   lambda {|char| links[char]}
+  # end
+  #
+  # def zoom_to_string(string)
+  #   chars = string.chars
+  #   chars.each do |char|
+  #     link_forward(char)
+  #   end
+  # end
 
 end
+
+completer = CompleteMe.new
--- test/complete_me_test.rb
+++ test/complete_me_test.rb
@@ -4,9 +4,53 @@ require 'pry'
 
 class CompleteMeTest < Minitest::Test
 
-  def test_class_exists
+  attr_reader :complete
+
+  def setup
+    @complete = CompleteMe.new("")
+  end
+
+  def test_it_exists
     assert CompleteMe.new
   end
 
+  def test_it_initializes_without_data
+    completed = CompleteMe.new
+    refute completed.root
+  end
+
+  def test_it_can_initialize_with_data
+    assert_equal "" , complete.root
+  end
+
+  def test_it_begins_with_alphabet_links
+    assert_equal({"a" => nil, "b" => nil} , complete.links)
+  end
+
+  def test_it_can_add_a_letter_word
+    complete.add_word("a")
+    assert_equal "a", complete.links["a"].root
+  end
+
+  def test_it_recognizes_letter_as_a_word
+    complete.add_word("a")
+    assert complete.links["a"].word
+  end
+
+  def test_it_can_add_a_two_letter_word
+    complete.add_word("ba")
+    assert_equal "ba", complete.links["b"].links["a"].root
+  end
+
+  def test_when_it_adds_a_two_letter_word_it_adds_the_first_letter_as_word
+    complete.add_word("ba")
+    assert_equal "b", complete.links["b"].root
+  end
+
+  def test_it_recognizes_two_letter_word_as_word_but_not_first_letter
+    complete.add_word("ba")
+    refute complete.links["b"].word
+    assert complete.links["b"].links["a"].word
+  end
 
 end
